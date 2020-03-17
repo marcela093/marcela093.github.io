2c21cd553389cb6de0a5ec6914e7eb1608dd63c2
marcela093 <marcela.souza093@gmail.com>
2020-03-16 22:03:12 -0300

    add event no form

--- consultaCep/js/script.js
+++ consultaCep/js/script.js
@@ -1,36 +1,35 @@
 const getElement = document.querySelector.bind(document);
 
-const button = getElement('#btn-send');
+const form = getElement('.form__container');
 const inputCep = getElement('#input-user');
 
-const street = getElement('#street');
-const complement = getElement('#complement');
-const neighborhood = getElement('#neighborhood');
-const city = getElement('#city');
-const state = getElement('#state');
-
-
-button.addEventListener('click', function(event){
+form.addEventListener('submit', function(event){
     event.preventDefault();
 
     getAdress();
-
+    
     inputCep.value = '';
 
-})
+});
 
 const getAdress = () => {
     const cep = getElement('#input-user').value;
 
+    const street = getElement('#street');
+    const complement = getElement('#complement');
+    const neighborhood = getElement('#neighborhood');
+    const city = getElement('#city');
+    const state = getElement('#state');
+
+
     fetch('https://viacep.com.br/ws/'+ cep +'/json/')
     .then(response => response.json())
     .then(data =>{
-        street.append(data.logradouro)
-        complement.append(data.complemento)
-        neighborhood.append(data.bairro)
-        city.append(data.localidade)
-        state.append(data.uf)
-
+        street.append(data.logradouro);
+        complement.append(data.complemento);
+        neighborhood.append(data.bairro);
+        city.append(data.localidade);
+        state.append(data.uf);
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
 }
